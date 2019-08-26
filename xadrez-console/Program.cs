7b6a2d6b5ab4022678bb264b47954a75466cda70
Gabriela Thalita <gabrielathalitasv@hotmail.com>
2019-08-26 18:23:17 -0300

    Iniciando testes em possiveis movimentos das pecas

---
+++ xadrez-console/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using tabuleiro;
+using xadrez;
+
+namespace xadrez_console {
+    class Program {
+        static void Main(string[] args) {
+
+            try {
+                PartidaDeXadrez partida = new PartidaDeXadrez();
+
+                while (!partida.terminada) {
+
+                    Console.Clear();
+                    Tela.imprimirTabuleiro(partida.tab);
+
+                    Console.WriteLine();
+                    Console.Write("Origem: ");
+                    Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
+
+                    bool[,] posicoesPossiveis = partida.tab.peca(origem).movimentosPossiveis();
+
+                    Console.Clear();
+                    Tela.imprimirTabuleiro(partida.tab, posicoesPossiveis);
+
+                    Console.WriteLine();
+                    Console.Write("Destino: ");
+                    Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
+
+                    partida.executaMovimento(origem, destino);
+                }
+
+            }
+            catch (TabuleiroException e) {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
